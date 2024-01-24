d6e66d5d1b67d85543f457e89ec37f602436d0e9
Pankaj <Pankaj@DESKTOP-BFUD1MF>
2024-01-24 19:01:08 +0530

    Initial set up with Components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Function from './components/Function.js'
+import Class from './components/Class.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Function></Function>
+      <Class></Class>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Function.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Function()
+{
+  return <h1>Function Component</h1>
+}
+
+export default Function;
